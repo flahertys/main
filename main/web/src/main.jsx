87bf3b5d8dd3c85af4e9876989c2dda4c4d2c27a
Michael O Flaherty <mike.flaherty@my.strayer.edu>
2026-03-09 20:17:10 -0400

    HOTFIX: Remove 'use client' directive causing white screen - add error boundary

--- main/web/src/main.jsx
+++ main/web/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import RootErrorBoundary from "./RootErrorBoundary.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RootErrorBoundary>
+      <App />
+    </RootErrorBoundary>
   </React.StrictMode>
 );
